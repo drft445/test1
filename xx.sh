094d9995119c838fafc39e4a38c83e666b098cfb
drft445 <71051104+drft445@users.noreply.github.com>
2021-02-02 20:11:20 +0000

    Update xx.sh

--- xx.sh
+++ xx.sh
@@ -1,9 +1,8 @@
-mycomb() {
-for i in ing.txt; do
-curl "$i" -s | grep -o "var [-A-Za-z0-9\_]*" | sed -e "s/var\s//" -e 's/^/\&/' -e 's/$/\=/' > x.txt
+for i in $(cat ing.txt); do
+
+curl "$i" -s | grep -o "var [-A-Za-z0-9\_]*" | sed -e "s/var\s//" -e 's/^/\&/' -e 's/$/\=x/' > x.txt
 echo "$i" > x1.txt
 comb x1.txt x.txt >> 111
 rm x1.txt x.txt
 done
-}
-mycomb
+
