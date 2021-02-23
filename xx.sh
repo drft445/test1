e73df657efd9ccd96e4815aefab3b1c382be7f05
drft445 <71051104+drft445@users.noreply.github.com>
2021-02-23 12:17:18 +0100

    Update xx.sh

--- xx.sh
+++ xx.sh
@@ -1,6 +1,7 @@
 for i in $(cat target); do
-curl "$i" -s | grep -o "var [-A-Za-z0-9\_]*" | sed -e "s/var\s//" -e 's/^/\&/' -e 's/$/\=x/' > x.txt
+curl "$i" -s | grep -o "var [-A-Za-z0-9\_]*\|name=[-A-Za-z0-9\_\'\"]*" | sed -e "s/var\s//" -e 's/^/\&/' -e 's/$/\=x/' -e "s/name=//" -e "s/'//g" -e "s/\"//g" > x.txt
 echo "$i" > x1.txt
 comb x1.txt x.txt >> target1
 rm x1.txt x.txt
 done
+
